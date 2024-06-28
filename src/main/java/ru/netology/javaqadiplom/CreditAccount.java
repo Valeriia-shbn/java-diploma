9d8e5c284c8709d54c4fcc404364f8b3f1e2dab7
YukhnikOlga <olga.iukhnik@gmail.com>
2024-06-29 00:28:19 +0300

    correct IllegalArgumentException

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -20,8 +20,19 @@ public class CreditAccount extends Account {
     public CreditAccount(int initialBalance, int creditLimit, int rate) {
         if (rate <= 0) {
             throw new IllegalArgumentException(
-                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+                    "Кредитная ставка не может быть отрицательной, а у вас: " + rate
             );
+        }
+            if (initialBalance <= 0) {
+                throw new IllegalArgumentException(
+                        "Начальный баланс не может быть отрицательным, а у вас: " + initialBalance
+                );
+            }
+        if (creditLimit <= 0) {
+            throw new IllegalArgumentException(
+                    "Кредитный лимит не может быть отрицательним, а у вас: " + creditLimit
+            );
+
         }
         this.balance = initialBalance;
         this.creditLimit = creditLimit;
